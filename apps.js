abd85040d73c6d5b7f9a0b9a06746376960feb56
WildcherryBazzaire <Eieghtbitdolphin@gmail.com>
2018-02-28 20:15:20 -1000

    still working on logic

--- apps.js
+++ apps.js
@@ -1,21 +1,55 @@
 //class the months
+console.log("Big Gay");
+var calender = document.getElementById("Calendar");
 
-var calender = document.getElementById("Calendar")
-
-var January = {
-	first: [1,"Sample Text","images/school.jpg"], //three array variable: date, Event Description, Image SRC
+var Januaryevents = {
+	first: [1,"Sample Text","images/school.jpg"], //three array variable: date, Event Description, Image SRC, Detailed Description
 	second: [2,"Sample Text","images/school.jpg"],
 	third: 	[3,"Sample Text","images/school.jpg"],
 	fourth: [4,"Sample Text","images/school.jpg"],
-	fifth: [5,"ur MoM gay LOL","images/dab.jpg"]
+	fifth: [5,"ur MoM gay LOL","images/dab.jpg","UR MoM iS NOW BiG Gay"]
 }
 
-for(var dates in January) {  //implement to function later...
-	console.log(January[dates][0]);
+function Overlay(description,clickedDay) {
+	var MoreDescript = document.newElement("DIV");
+	MoreDescript.className = "MoreInfo"; //class Name for overlays
+	MoreDescript.innerHTML = description;
+}
+
+function MonthDays(days,events){
+	for(var i=1; i < days; i++) {  //implement to function later...
+		
+		var addInDays = document.createElement("DIV"); //for days
+		var addInDescript = document.createElement("DIV"); //for the description in the divs
+		
+		//adds the date
+		addInDays.className = "Days"; //class name is Days
+		calender.appendChild(addInDays); //adds in the days
+		
+		//adds the Description & DIV for the image
+		addInDescript.className = "Descriptions"; //class name is Descriptions
+		addInDays.appendChild(addInDescript);
+		
+		for(var j in events){ // logic to add in stuff
+			if(events[j][0] === i){
+				addInDescript.innerHTML = events[j][1];
+			}
+			/*if(events[j][3] !== undefined){
+				overlay(events[j][3],getElementsByClassName("addInDays")[i];
+			} */
+		} 
+	}
+}
+
+MonthDays(31,Januaryevents);
+
+/*
+console.log(January[dates][0]);
 	
 	var addIn = document.createElement("DIV");
 	var addInDescript = document.createElement("DIV");
 	var addInImage = document.createElement("IMG");
+	var days = 31
 	
 	//adds the date
 	addIn.className = "Days"; //class name is Days
@@ -33,6 +67,4 @@ for(var dates in January) {  //implement to function later...
 		addInImage.src = January[dates][2];
 		addInDescript.appendChild(addInImage);
 	}
-}
-
-console.log(calender.childNodes);
+*/
