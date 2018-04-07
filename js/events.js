c82964cac20b198539fe7c941945ce19e64c2c70
WildcherryBazzaire <Eieghtbitdolphin@gmail.com>
2018-04-06 20:43:32 -1000

    Fix the master

---
+++ js/events.js
@@ -0,0 +1,90 @@
+var events = { //every event is now in the array
+	2018: {
+		year: 2018,
+		January: {
+			month: 1, //says what month it is
+			first: [1,"Sample Text",{
+				title: "Good Update!",
+				header1: "oof",
+				par1: "addedd the overlay",
+				img1: "images/school.jpg",
+				header2: "wowzers",
+				par2: "plus this cool flexible feature, fits mulitple items instead of one",
+				img2: "images/To women.jpg",
+				header3: "insane!",
+				par3: "just make sure you give keys there respective names",
+				img3: "images/layermask.png"
+			},1],												 //three array variable: date,month,Event Description,Detailed Description
+			job5: [5,"ur MoM gay LOL", {
+				title: "reason why ur MOM big Gay",
+				par1: "No U",
+				img1: "images/dab.jpg"
+			}],
+			sixth: [6,"Holla if you here me", {
+				title: "Kicked him in the head when 25",
+				header1: "got spots",
+				par1: "funky little boat race",
+				header2: "boogaloo dudes"
+			}]
+		},
+		Febuary: {
+			month: 2
+		},
+		March: {
+			month: 3,
+			NationalPigDay: [1,"Animals rise once more", {
+				title: "Extra: Napoleon kicks Snowball fom Animal Farm, is new leader",
+				header1: "The message painted on the barn wall",
+				par1: "All Animals our equal, but some are more equal than others"
+			}],
+			UrMomGayDay: [26,"Jesus will 88", {
+				title: "Buddha",
+				header: "Buddha isn't fat",
+				par1: "if he was, then the Earth would be round"
+			}]
+		},
+		April: {
+			month: 4,
+			OOF: [27,"THis Should Work", {
+				title: "good images",
+				img1: "https://www.hdwallback.net/wp-content/uploads/2017/03/4K-High-Definition-Wallpaper-3840x2160-680x425.jpg"
+			}]
+		},
+		June: {
+			month: 5
+		},
+		May: {
+			month: 6
+		},
+		July: {
+			month: 7
+		},
+		August: {
+			month: 8
+		},
+		September: {
+			month: 9
+		},
+		October: {
+			month: 10
+		},
+		November: {
+			month: 11
+		},
+		December: {
+			month: 12
+		}
+	},
+	2019: {
+		year: 2019,
+		January: {
+			month: 1,
+			Rutles: [1,"The Rutles Live Concert", {
+				title: "THe Rutles & Beatles Live in concert",
+				header1: "Whats being Natural?",
+				par1: "Ultimate combo to which your monf would explode",
+				img1: "images/To women.jpg"
+			}]
+		}
+	}
+}
