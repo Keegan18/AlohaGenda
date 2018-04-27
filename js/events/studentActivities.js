30c7814ca9e9d02b9d230bba59b8874bf084cf72
MarkIsNotTaken <mdemesillo@gmail.com>
2018-04-27 13:25:55 -1000

    added events

--- js/events/studentActivities.js
+++ js/events/studentActivities.js
@@ -1,3 +1,17 @@
 var studentActivities = {
-	
+	2018: {
+    year: 2018,
+    April: {
+      month: 4,
+      SpringFair: [30, "Spring Fair", {
+        title: "Spring Fair",
+        header1: "Get your money ready!",
+        par1: "Lots of Foods and snacks to eat and enjoy",
+        img1: ""
+      
+      }],
+   
+    }
+  
+  }
 };
